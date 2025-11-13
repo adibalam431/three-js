474da4c96d25a9a2cc7ead5392a8bf694b4458e3
adibalam431 <adibalam431@gmail.com>
2025-11-13 13:54:57 +0530

    animation

--- three.js
+++ three.js
@@ -1,5 +1,4 @@
 //scene
-
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, .1, 100);
 camera.position.z=5;
@@ -8,11 +7,20 @@ scene.add(camera);
 let box = new THREE.BoxGeometry(1,1,1);
 let material = new THREE.MeshBasicMaterial({color:"red"});
 let mesh = new THREE.Mesh(box,material);
-mesh.position.x=2
 scene.add(mesh);
 
 const canvas = document.querySelector("canvas")
-let renderer = new THREE.WebGLRenderer({canvas});
+let renderer = new THREE.WebGLRenderer({canvas,antialias:true});
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 renderer.render(scene,camera)
+
+//animation
+
+let clock = new THREE.Clock();
+function animate(){
+    window.requestAnimationFrame(animate);
+    renderer.render(scene,camera);
+    mesh.rotation.y = clock.getElapsedTime();
+}
+animate();
